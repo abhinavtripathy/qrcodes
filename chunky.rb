ee72c0912735fe73f3c97f59c6aea8b4f4040604
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-11-08 15:28:45 -0500

    Minor

--- chunky.rb
+++ chunky.rb
@@ -2,5 +2,5 @@ require 'chunky_png'
 
 avatar = ChunkyPNG::Image.from_file('abc.png')
 badge  = ChunkyPNG::Image.from_file('glogo.png')
-avatar.compose!(badge, 200, 200)
+avatar.compose!(badge, 10, 10)
 avatar.save('composited.png', :fast_rgba)
